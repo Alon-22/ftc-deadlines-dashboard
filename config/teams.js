0d9e89ad34c56cbecb18db906e6c79076d0924d1
Alon <45106871+Alon-22@users.noreply.github.com>
2026-09-01 11:23:47 -0700

    Update teams.js

--- config/teams.js
+++ config/teams.js
@@ -6,9 +6,9 @@
 // and never stored in this file or in git.
 
 window.TEAMS = [
-  // {
-  //   key: 'example',
-  //   label: 'Example FTC Team',
-  //   webAppUrl: 'https://script.google.com/macros/s/XXXXXXXXXXXX/exec',
-  // },
+  {
+     key: 'MysteryMeat',
+     label: 'MysteryMeat 20406',
+     webAppUrl: 'https://script.google.com/macros/s/AKfycbxXfLyvkWw02q770kdCzF_Pfjju33NWMm7qUqb98Pb9xiEedGFAXEh9nK53VaLiawR8_A/exec',
+   },
 ];
